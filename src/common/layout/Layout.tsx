88e6a52f552d3ea5ddc7edc6b5d0474b7528d45c
jm4293 <dlwoals4293@gmail.com>
2025-03-17 22:56:39 +0900

    [Design]: layout CSS 수정, 모바일에서는 사이드바, 데스크탑에서는 헤더

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Sidebar } from '@/common/layout/Sidebar';
+import { useDeviceLayout } from '@/hooks/useDeviceLayout';
+import { Header } from '@/common/layout/Header';
 
 export const Layout = () => {
+  const { isMobile } = useDeviceLayout();
+
   return (
     <div className="layout">
+      <div className="header-container">{isMobile ? <Sidebar /> : <Header />}</div>
       <div className="outlet">
         <Outlet />
       </div>
---
+++ src/pages/user/UserList.tsx
@@ -0,0 +1,3 @@
+export const UserList = () => {
+  return <div>user</div>;
+};
